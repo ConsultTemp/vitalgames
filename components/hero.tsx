032838ae5f6e32629ff43e4e7d92a7e6a77fb805
Siva24Team <114410356+LeonardoCahuas@users.noreply.github.com>
2025-05-29 17:10:26 +0200

    .

--- components/hero.tsx
+++ components/hero.tsx
@@ -5,8 +5,8 @@ export default function HeroSection() {
   return (
     <section className="relative w-full h-fit bg-black overflow-hidden">
       <OptimizedVideo
-        src="https://files.catbox.moe/9muvk1.webm"
-        mobileSrc="https://files.catbox.moe/8oznbj.webm"
+        src="https://files.catbox.moe/vtza20.webm"
+        mobileSrc="https://files.catbox.moe/99ij7f.webm"
         containerClassName="w-full h-full"
         className="w-full h-full object-cover"
         autoPlay={true}
