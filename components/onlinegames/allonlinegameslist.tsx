ac71007fb1519828dbe888ba4256ff178ab87607
Siva24Team <114410356+LeonardoCahuas@users.noreply.github.com>
2025-11-26 15:59:41 +0100

    .

--- components/onlinegames/allonlinegameslist.tsx
+++ components/onlinegames/allonlinegameslist.tsx
@@ -50,7 +50,9 @@ export default function AllOnlineGamesList({ lang }: AllOnlineGamesListProps) {
                   
                   {/* Play Demo Button */}
                   <Link
-                    href={`/onlinegames/${game.id}`}
+                    href={game.demoLink}
+                    target="_blank"
+                    rel="noopener noreferrer"
                     className="inline-block w-full bg-none border border-white text-vitalYellow hover:border-vitalYellow hover:bg-vitalYellow hover:text-black font-medium py-2 px-6 rounded-full transition-colors duration-300 text-md md:text-lg whitespace-nowrap flex items-center justify-center gap-2"
                   >
                     <PlayIcon className="w-4 h-4" fill="currentColor"/>
